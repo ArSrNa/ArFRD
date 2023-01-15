783cce8b60b2f08a007f809f5ad32bd306d101c9
ArSrNa <arsrna.cn@outlook.com>
2023-01-15 13:23:18 +0800

    demo内置方法

--- demo.js
+++ demo.js
@@ -1,3 +1,22 @@
+function specific(exp,name,times,bdArgs){
+    //powered by Ar-Sr-Na
+    //概率,保留小数位,友好名称,次数,保底参数{time:次数}
+    var num=0
+    for(var i1=0;i1<times;i1++){
+        var rds=parseFloat((Math.random()*100));
+        if(rds<=exp){
+            num++
+        }else if(num==0 && (i1+1)==bdArgs.times){
+            num++
+        }
+    }
+    return({
+        "name":name,
+        "times":num,
+        "probable":parseFloat(num/times)
+    })
+ }
+
 function demo(){
     var result={
         S级角色卡:0,
